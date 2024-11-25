e036f3159303ac7fa3616e8d3ca37166c00fd45d
Erisnilton Freitas <erisniltongf@gmail.com>
2024-11-25 19:57:12 -0300

    deploy do projeto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import unocss from "@unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
+    base: 'Produto-Educacional',
     outDir: 'docs',
     integrations: [
         unocss({
