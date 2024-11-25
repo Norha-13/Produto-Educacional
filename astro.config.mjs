a332a8eb73578e642762dc97d8bac29e6efd171f
Erisnilton Freitas <erisniltongf@gmail.com>
2024-11-25 18:57:37 -0300

    deploy do projeto

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import unocss from "@unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+    outDir: 'docs',
+    integrations: [
+        unocss({
+            injectReset: true
+        })
+    ]
+});
---
+++ uno.config.ts
@@ -0,0 +1,48 @@
+
+import presetWebFonts from '@unocss/preset-web-fonts'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+import { defineConfig, presetUno, type PresetUnoTheme } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig<PresetUnoTheme>({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      fonts: {
+        "display": ['Inter'],
+        "sans": ['Open Sans'],
+      },
+    })
+  ],
+  theme: {
+    colors: {
+      primary: 'blue-600',
+      "primary-foreground": '#FFF',
+      foreground: '#000000',
+      "foreground-foreground": '#FFF',
+      muted: '#D0CDCD',
+      background: '#ffffff',
+    },
+    container: {
+      center: true,
+      maxWidth: {
+        '2xl': '1200px',
+        'xl': '1050px',
+        'lg': '1024px',
+        'md': '768px',
+        'sm': '640px',
+        'xs': '480px'
+      }
+    }
+  },
+  shortcuts: [
+    ["text-title-1", "font-display text-4 md:text-8 font-800 leading-12 tracking-0.2px"],
+    ["text-title-2", "font-display text-2 md:text-6 font-800 leading-12 tracking-0.2px"],
+    ["text-body", "font-sans text-4 font-400 leading-6 tracking-0.2px"],
+    ["text-body-md", "font-sans text-4 md:text-5 font-400 leading-6 tracking-0.2px"],
+  ],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives()
+  ]
+})
